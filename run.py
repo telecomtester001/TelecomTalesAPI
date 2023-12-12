aaa6fb4768a8d26c41cc8c297a249077d621002d
telecomtester001 <152339706+telecomtester001@users.noreply.github.com>
2023-12-12 19:14:48 +0000

    Manual setup for swagger ui for api documentation

--- run.py
+++ run.py
@@ -1,9 +1,37 @@
+
 from telecomtalesapi import app, db
-from telecomtalesapi.models import *
 from flask_migrate import Migrate
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask import send_from_directory
+#from flask import Flask
+
+from telecomtalesapi.models import *
+
+#app = Flask(__name__)  # Create the Flask application instance
+
+# Configure SwaggerUI
+SWAGGER_URL = ''  # Expose Swagger UI at the root
+API_URL = '/documentation/swagger.yaml'  # API url to the swagger file
+
+# Register the Swagger UI blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Empty string means root
+    API_URL,
+    config={
+        'app_name': "Telecom Tales API"
+    },
+)
+
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+
+@app.route('/documentation/<path:path>')
+def send_documentation(path):
+    # Serve files for Swagger UI.
+    return send_from_directory('documentation', path)
 
 # Configure Flask-Migrate
 migrate = Migrate(app, db)
+
 
 if __name__ == '__main__':
     app.run(debug=True)  # debug to False for production environment
--- telecomtalesapi/__init__.py
+++ telecomtalesapi/__init__.py
@@ -2,14 +2,23 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from flask_httpauth import HTTPBasicAuth
+from flask_cors import CORS
 from config import DevelopmentConfig
 
 app = Flask(__name__)  # Create the Flask application instance
 app.config.from_object(DevelopmentConfig)  # Load configuration
 
 db = SQLAlchemy(app)  # Initialize SQLAlchemy with the Flask app
+migrate = Migrate(app, db)
+CORS(app)# Enable CORS for all routes
 auth = HTTPBasicAuth()  # Initialize auth
+
+
+# Import models and routes after the app and extensions have been initialized
+from .models import *
+from .routes import *
 
 from .auth import verify_password  # Importing auth-related functions
 
